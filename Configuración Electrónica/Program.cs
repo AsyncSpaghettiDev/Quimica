31df0f50bf141afa59585b9d325e92d0bd0a1e54
JonathanMov <jafetmv21@gmail.com>
2020-02-26 19:51:52 -0800

    Calcula config electrónica

---
+++ Configuración Electrónica/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+    class Program{
+        static void Main(){
+            String[] nivel = new String[4]{"s","p","d","f"};
+            Int16[] carga = new Int16[]{2,2,6,2,6,2,10,6,2,10,6,2,14,10,6,2,14,10,6};
+
+            Console.Write("¿Cuántos electrónes tiene?: ");
+            Int16 e=Int16.Parse(Console.ReadLine());
+
+            int n;
+            for(int i=1;i<=7;i++){
+                for(int j=0;j<4;j++){
+                    for(int k=0;k<=e;k++){
+                        n=carga[k];
+                        Console.Write("{0}{1}{2} ",i,nivel[j],n);
+                        if (n==e) break;
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
