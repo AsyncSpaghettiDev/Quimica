2a873e90eea0a2f8c5bffb9effb947cb8a0c6cb3
JonathanMov <jafetmv21@gmail.com>
2020-02-26 20:33:37 -0800

    Imprimir arbol electrónico

--- Configuración Electrónica/Program.cs
+++ Configuración Electrónica/Program.cs
@@ -1,23 +1,23 @@
 ﻿using System;
 
     class Program{
+        int Config(int e){
+            for(int i=0)
+        }
         static void Main(){
-            String[] nivel = new String[4]{"s","p","d","f"};
-            Int16[] carga = new Int16[]{2,2,6,2,6,2,10,6,2,10,6,2,14,10,6,2,14,10,6};
+            String[,] nivel = new String[7,4] {{"1s","2s","",""},{"2p","3s","",""},{"3p","4s","",""},{"3d","4p","5s",""},{"4d","5p","6s",""},{"4f","5d","6p","7s"},{"5f","6d","7p",""}};
+            Int16[,] carga = new Int16[7,4] {{2,2,0,0},{6,2,0,0},{6,2,0,0},{10,6,2,0},{10,6,2,0},{14,10,6,2},{14,10,6,0}};
 
             Console.Write("¿Cuántos electrónes tiene?: ");
             Int16 e=Int16.Parse(Console.ReadLine());
 
-            int n;
-            for(int i=1;i<=7;i++){
+            for(int i=0;i<7;i++){
                 for(int j=0;j<4;j++){
-                    for(int k=0;k<=e;k++){
-                        n=carga[k];
-                        Console.Write("{0}{1}{2} ",i,nivel[j],n);
-                        if (n==e) break;
-                    }
-                    Console.WriteLine();
+                    if (carga[i,j]==0) break;
+                    Console.Write("{0}{1} ",nivel[i,j],carga[i,j]);
                 }
+                Console.WriteLine();
             }
+            
         }
     }
