3109e8b2a43408f66c9221036dcb4b11355cef73
JonathanMov <jafetmv21@gmail.com>
2020-02-26 20:36:38 -0800

    Atributos publicos

--- Configuración Electrónica/Program.cs
+++ Configuración Electrónica/Program.cs
@@ -1,13 +1,8 @@
 ﻿using System;
-
     class Program{
-        int Config(int e){
-            for(int i=0)
-        }
+        String[,] nivel = new String[7,4] {{"1s","2s","",""},{"2p","3s","",""},{"3p","4s","",""},{"3d","4p","5s",""},{"4d","5p","6s",""},{"4f","5d","6p","7s"},{"5f","6d","7p",""}};
+        Int16[,] carga = new Int16[7,4] {{2,2,0,0},{6,2,0,0},{6,2,0,0},{10,6,2,0},{10,6,2,0},{14,10,6,2},{14,10,6,0}};
         static void Main(){
-            String[,] nivel = new String[7,4] {{"1s","2s","",""},{"2p","3s","",""},{"3p","4s","",""},{"3d","4p","5s",""},{"4d","5p","6s",""},{"4f","5d","6p","7s"},{"5f","6d","7p",""}};
-            Int16[,] carga = new Int16[7,4] {{2,2,0,0},{6,2,0,0},{6,2,0,0},{10,6,2,0},{10,6,2,0},{14,10,6,2},{14,10,6,0}};
-
             Console.Write("¿Cuántos electrónes tiene?: ");
             Int16 e=Int16.Parse(Console.ReadLine());
 
